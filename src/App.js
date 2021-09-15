9cd29645baa922f8b5f4c4f87ae6219d34780541
danieleum <66753724+danieleum@users.noreply.github.com>
2021-09-15 16:59:26 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import MazeSolver from './board/MazeSolver';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="nav-bar">
+        <h1> Maze Solver </h1>
+      </div>
+      <MazeSolver />
+    </div>
+  );
+}
+
+
+
+export default App;
